8776894633b1fc4ad6526032273d738cf023556a
darcychung <darcycchung@gmail.com>
2018-07-11 18:19:47 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,10 +1,23 @@
+rainbow_colors = [
+  "yellow"
+  "default"
+  "light_cyon"
+  ]
 def change_rainbow_colors
-rainbow_colors = ["red", "light red", "light yellow"]
+rainbow_colors = [
+"red"
+"light_red"
+"light_yellow"
+]
 end
 puts change_rainbow_colors
 
 def add_colors
-rainbow_colors = ["red", "light red", "light yellow"]
+rainbow_colors = [
+"red"
+"light_red"
+"light_yellow"
+]
 rainbow_colors.push "green", "blue"
 end
 puts add_colors
